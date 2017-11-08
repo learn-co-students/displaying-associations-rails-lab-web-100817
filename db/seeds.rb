91a2490bb40e114f44463578b0a2365945716f18
Gonzodamus <Dick@TheDickWard.com>
2017-11-08 11:42:09 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+davi = Artist.create(name: "Robert Davi")
+ davi.songs.create(title: "The best song ever")
+ davi.songs.create(title: "Second best song")
+ davi.songs.create(title: "Worst song")
+
+ rober = Artist.create(name: "David Rober")
+  rober.songs.create(title: "The David Song")
+  rober.songs.create(title: "Song for David")
+  rober.songs.create(title: "David is the best.")
