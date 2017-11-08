18c5d8ce6d111aaf17570a0e3be0cb1ec6229927
MatteBru <mbruni616@gmail.com>
2017-11-08 11:07:50 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+a = Artist.create(name: "billy the kid")
+b = a.songs.build(title: "songsongsong")
+b.save
