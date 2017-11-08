2e7b7d9b14d38b70c270aaf32f127250278e3045
github username <github email address>
2017-11-08 11:18:43 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -42,6 +44,8 @@ class ArtistsController < ApplicationController
     redirect_to artists_path
   end
 
+  def song_count
+  end
   private
 
   def artist_params
